c4c3a141529897fb09ab7f1897b6ecdb288d6a96
Mikulew <mikulew@gmail.com>
2019-04-13 13:00:50 +0200

    [#3.1] Fix slot in BaseButton

--- src/layout/settings/Settings.vue
+++ src/layout/settings/Settings.vue
@@ -2,8 +2,12 @@
     <div class="row">
         <h1>Settings</h1>
         <div class="col-12">
-            <BaseButton :loading="false" @click="logChange()" />
-            <BaseButton :loading="true" @click="logChange()" />
+            <BaseButton :loading="false" @click="logChange()">
+                <span>BaseButton first</span>
+            </BaseButton>
+            <BaseButton :loading="true" @click="logChange()">
+                <span>BaseButton second</span>
+            </BaseButton>
         </div>
         <div class="col-12 mt-3">
             <BaseInput label="test" />
