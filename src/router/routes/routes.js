a63a83d561109dc23870b6c207b2ce1e7585df33
Mikulew <mikulew@gmail.com>
2019-04-13 10:19:17 +0200

    [#1.1] Routing settings

--- src/router/routes/routes.js
+++ src/router/routes/routes.js
@@ -1,15 +1,27 @@
-import HelloWorld from '../../components/HelloWorld.vue';
-import Register from '../../components/Register.vue';
+import Home from '../../components/home/Home.vue';
+import Vote from '../../components/vote/Vote.vue';
+import Score from '../../components/score/Score.vue';
+import Settings from '../../components/settings/Settings.vue';
 
 export default [
     {
         path: '/',
-        name: 'HelloWorld',
-        component: HelloWorld,
+        name: 'Home',
+        component: Home,
     },
     {
-        path: '/register',
-        name: 'Register',
-        component: Register,
+        path: '/vote',
+        name: 'Vote',
+        component: Vote,
+    },
+    {
+        path: '/score',
+        name: 'Score',
+        component: Score,
+    },
+    {
+        path: '/settings',
+        name: 'Settings',
+        component: Settings,
     }
 ];
