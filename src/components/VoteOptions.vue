be92fd0b03f3bdcf22d5b32da1feb62e273f1101
Mikulew <mikulew@gmail.com>
2019-04-13 13:31:49 +0200

    [#3.2] Add VodeOptions

---
+++ src/components/VoteOptions.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <slot v-for="option in options" :options="option">{{ option }}</slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        options: {
+            type: Object,
+            required: false,
+            default: () => ([
+                {
+                    name: 'AAA',
+                    surname: 'BBB',
+                },
+                {
+                    name: 'CCC',
+                    surname: 'DDD',
+                }
+            ]),
+        },
+    }
+}
+</script>
--- src/layout/vote/Vote.vue
+++ src/layout/vote/Vote.vue
@@ -1,5 +1,35 @@
 <template>
     <div>
-        <h1>Vote</h1>
+        <VoteOptions :options="users" v-slot="{options}">
+            <h1>{{ options }}</h1>
+        </VoteOptions>
     </div>
 </template>
+
+<script>
+import VoteOptions from '../../components/VoteOptions.vue';
+
+export default {
+    data() {
+        return {
+            users: [
+                {
+                    name: 'Jan',
+                    surname: 'Kowalski'
+                },
+                {
+                    name: 'Beata',
+                    surname: 'Wiolczak',
+                },
+                {
+                    name: 'Damian',
+                    surname: 'Lalski',
+                }
+            ],
+        };
+    },
+    components: {
+        VoteOptions,
+    },
+}
+</script>
